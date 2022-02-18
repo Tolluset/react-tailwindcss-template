aa22f08fce1dacc2405c3ab7176f5760b0c142da
tolluset <dlqud19@gmail.com>
2022-02-18 20:14:28 +0900

    chore: :package: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <h1>React</h1>
+      <h1 className="m-3 bg-blue-500 p-2 text-green-300">React</h1>
       <p>How to setting the project</p>
     </>
   );
